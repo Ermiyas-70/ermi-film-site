17f9c973d3e82ff6ecb759699b30eb6c9aa884a3
Ermiyas-70 <ermiyastomas70@gmail.com>
2026-09-18 11:39:40 +0300

    ubdated index.js

--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
 import app from "./server.js"
 import mongodb from "mongodb"
-import ReviewsDAO from "./dao/reviewsDAO.jS"
+import ReviewsDAO from "./dao/reviewsDAO.js"
 import dotenv from "dotenv"
 
 dotenv.config()
@@ -11,16 +11,23 @@ const mongo_password = process.env['MONGO_PASSWORD']
 
 const uri = `mongodb+srv://${mongo_username}:${mongo_password}@cluster0.iaaqbxv.mongodb.net/?appName=Cluster0`
 
-let isConnected = false
+const port = process.env.PORT || 8000
 
-async function connectDB() {
-  if (isConnected) return
-  const client = await MongoClient.connect(uri, { maxPoolSize: 50 })
-  await ReviewsDAO.injectDB(client)
-  isConnected = true
-}
+MongoClient.connect(
+  uri,
+  {
+    maxPoolSize: 50,
+    wtimeoutMS: 2500,
+  }
+)
+  .catch(err => {
+    console.error(err.stack)
+    process.exit(1)
+  })
+  .then(async client => {
+    await ReviewsDAO.injectDB(client)
 
-export default async function handler(req, res) {
-  await connectDB()
-  return app(req, res)
-}
+    app.listen(port, () => {
+      console.log(`listening on port ${port}`)
+    })
+  })
