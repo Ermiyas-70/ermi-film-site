6c5b81a9591c326442b8090ff93e88f1ab2084f6
Ermiyas-70 <ermiyastomas70@gmail.com>
2026-09-18 12:14:11 +0300

    updated movi.js

--- frontend/movie.js
+++ frontend/movie.js
@@ -2,7 +2,7 @@ const url = new URL(location.href);
 const movieId = url.searchParams.get("id");
 const movieTitle = url.searchParams.get("title");
 
-const APILINK = 'http://localhost:8000/api/v1/reviews/';
+const APILINK = 'https://ermi-film-site-capw.vercel.app/';
 
 const main = document.getElementById("section");
 const title = document.getElementById("title");
